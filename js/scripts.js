dde1180ddb3841e2fc26d5ddf427b22a7beda51b
akikuno <akikuno@users.noreply.github.com>
2024-10-06 06:08:59 +0900

    Remove `copyLinkToClipboard`: タブ切り替えのリンクはうまくいかないので、該当の関数を削除しました

--- js/scripts.js
+++ js/scripts.js
@@ -8,25 +8,3 @@ function gtag() {
 }
 gtag("js", new Date());
 gtag("config", "G-29Y2VM34B3");
-
-// ========================================
-// h1タグにリンクを付ける関数
-// ========================================
-
-function copyLinkToClipboard(anchorId) {
-    if (document.getElementById(anchorId)) {
-        const tabContent = document.getElementById(anchorId).closest('.tabcontent');
-        if (tabContent) {
-            const tabName = tabContent.id;
-            openTab(null, tabName);
-        }
-    }
-
-    const url = window.location.href.split("#")[0] + "#" + anchorId;
-
-    navigator.clipboard.writeText(url).then(function () {
-        alert("Link copied to clipboard: " + url);
-    }).catch(function (err) {
-        console.error("Failed to copy text: ", err);
-    });
-}
