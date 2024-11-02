222ea96123567eba03a6b53b3253392031192fa2
Akihiro Kuno <akikuno@users.noreply.github.com>
2024-11-02 13:59:35 +0900

    Add Google analytics

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,1 @@
-// ========================================
-// Google Analytics関係
-// ========================================
 
-window.dataLayer = window.dataLayer || [];
-function gtag() {
-    dataLayer.push(arguments);
-}
-gtag("js", new Date());
-gtag("config", "G-29Y2VM34B3");
